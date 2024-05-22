645e13b1dd2330fc31a69442f9e8e0ac2a707289
TetieWasTaken <tjvssr@gmail.com>
2024-05-22 20:01:35 +0200

    Tweaked values

--- Scenes/Mandelbrot Sprite.gd
+++ Scenes/Mandelbrot Sprite.gd
@@ -1,9 +1,9 @@
 extends Sprite2D
 
-@export var zoom : float = 2.5
-@export var m_offset : Vector2 = Vector2(0.26, 0.0)
+@export var zoom : float = 0.4
+@export var m_offset : Vector2 = Vector2(-0.7, 0.0)
 @export var max_iterations : int = 100
-@export var scrolling_speed : float = 0.1
+@export var scrolling_speed : float = 0.2
 
 func _process(delta : float) -> void:
 	material.set("shader_parameter/zoom", zoom)
@@ -12,11 +12,11 @@ func _process(delta : float) -> void:
 	
 	if Input.is_action_just_pressed("zoom_out"):
 		print("Zoom out")
-		zoom *= 1.1
+		zoom /= 1.1
 	elif Input.is_action_just_pressed("zoom_in"):
 		print("Zoom in")
-		zoom /= 1.1
+		zoom *= 1.1
 	
-	var direction = Input.get_vector("move_right", "move_left", "move_down", "move_up")
+	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	if direction != Vector2.ZERO:
-		m_offset += direction * scrolling_speed * delta
+		m_offset += direction * scrolling_speed * delta / zoom
