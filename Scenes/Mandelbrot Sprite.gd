abd8bef5e9ed3ab0823b37385f574526a034d006
TetieWasTaken <tjvssr@gmail.com>
2024-05-22 20:17:24 +0200

    Zoom scaling

--- Scenes/Mandelbrot Sprite.gd
+++ Scenes/Mandelbrot Sprite.gd
@@ -5,17 +5,20 @@ extends Sprite2D
 @export var max_iterations : int = 100
 @export var scrolling_speed : float = 0.2
 
+@export var zoom_speed : float = 1
+@onready var zoom_target = zoom
+
 func _process(delta : float) -> void:
 	material.set("shader_parameter/zoom", zoom)
 	material.set("shader_parameter/offset", m_offset)
 	material.set("shader_parameter/max_iterations", max_iterations)
 	
-	if Input.is_action_just_pressed("zoom_out"):
-		print("Zoom out")
-		zoom /= 1.1
-	elif Input.is_action_just_pressed("zoom_in"):
-		print("Zoom in")
-		zoom *= 1.1
+	if Input.is_action_pressed("zoom_out"):
+		zoom_target -= 1.02 * delta * zoom
+	elif Input.is_action_pressed("zoom_in"):
+		zoom_target += 1.02 * delta * zoom
+	
+	zoom = move_toward(zoom, zoom_target, zoom_speed * delta * zoom)
 	
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	if direction != Vector2.ZERO:
